e1fa2810990619ae93f6cb1d4f7d84d19eb4ca69
shrouk <shrouk.passiony@gmail.com>
2016-05-09 23:19:12 +0200

    layout and course CRUD By Shrouk

---
+++ elearning_zend/application/controllers/CourseController.php
@@ -0,0 +1,79 @@
+<?php
+
+class CourseController extends Zend_Controller_Action
+{
+
+	private $model;
+    public function init()
+    {
+        /* Initialize action controller here */
+        $this->model = new Application_Model_DbTable_Course();
+    }
+
+    public function indexAction()
+    {
+         $this->view->courses = $this->model->listCourses();
+    }
+
+    public function addAction()
+    {
+    	$auth = Zend_Auth::getInstance();
+        if($auth->hasIdentity()){
+            $identity = $auth->getIdentity(); 
+            $user_id = $identity->id;
+ 
+    	   	$form= new Application_Form_Course();
+            // $form->removeElement('cimage');
+	        if($this->getRequest()->isPost()) {
+	            if ($form->isValid($this->getRequest()->getParams())) {
+	                $course_data=$form->getValues();
+	                if($this->model->addCourse($course_data,$user_id)){
+	                    
+	                    $this->redirect("course/index");
+	                }
+	            }
+	        }
+	        $this->view->form = $form;
+	    }
+    }
+
+    public function singleAction()
+    {
+    	//Get a single Course//
+        $id = $this->getRequest()->getParam('course_id');
+        $this->view->course = $this->model->getCourseById($id);
+
+    }
+
+    public function deleteAction()
+    {
+        //Delete a single Course//
+        $id = $this->getRequest()->getParam('id_cours');
+        if($this->model->deleteCourse($id))
+            $this->redirect('course/index');
+    }
+    
+    function editAction(){
+
+        //Update a single Course//
+        $id = $this->getRequest()->getParam('id_cours');
+        $course = $this->model->getCourseById($id);
+        $form = new Application_Form_Course();
+
+        $form->populate($course[0]);
+        //$values = $this->getRequest()->getParams();
+        if($this->getRequest()->isPost()){
+            if($form->isValid($this->getRequest()->getParams())){
+                $data = $form->getValues();
+                $data['id_cours'] = $id;
+                // var_dump($data);die;
+                $this->model->addCourse($data);
+                $this->redirect('course/'); 
+            }   
+        }
+        
+        $this->view->form = $form;
+        $this->render('add');
+    }
+
+}
---
+++ elearning_zend/application/forms/Course.php
@@ -0,0 +1,39 @@
+<?php
+
+class Application_Form_Course extends Zend_Form
+{
+
+    public function init()
+    {
+        /* Form Elements & Other Definitions Here ... */
+
+		$course = new Zend_Form_Element_Text('course');
+		$course->setLabel('Course');
+		$course->setRequired();
+
+		$cours_image = new Zend_Form_Element_File('cours_image');
+		$cours_image->setLabel('Course Image');
+		$cours_image->setDestination('/var/www/html/course/public/images/courses/');
+		// $cours_image->setRequired();
+
+		// $cimage = new Zend_Form_Element_Image('cours_image');
+		// $cimage->setLabel('Course Image');
+		// $cimage->setAttribute('src', '/var/www/html/course/public/images/courses/2.jpg');
+
+		$cours_desc= new Zend_Form_Element_Textarea('cours_desc');
+		$cours_desc->setLabel('Course Description');
+		$cours_desc->setRequired();
+
+		$id_cato = new Zend_Form_Element_Hidden('id_cato');
+		// $id_user = new Zend_Form_Element_Hidden('id_user');
+
+		$submit = new Zend_Form_Element_Submit('Save');
+		$submit->setAttrib('class', 'btn btn-info');
+
+
+		$this->addElements(array($course,$cours_image,$cours_desc,$id_cato,$submit ));
+    }
+
+
+}
+
