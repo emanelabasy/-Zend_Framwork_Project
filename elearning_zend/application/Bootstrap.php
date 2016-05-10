c5c036229d205dce8193e3f7c533c330fa527c97
shrouk <shrouk.passiony@gmail.com>
2016-05-10 09:26:15 +0200

    layout by shrouk

--- elearning_zend/application/Bootstrap.php
+++ elearning_zend/application/Bootstrap.php
@@ -7,17 +7,30 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 	{
 		$this->bootstrap('View');
 		$view = $this->getResource('View');
-		$view->doctype('XHTML1_STRICT');
+		$view->doctype('HTML5');
 		//Meta
 		$view->headMeta()->appendName('keywords', 'framework, PHP')->appendHttpEquiv('Content-Type','text/html;charset=utf-8');
 		// Set the initial title and separator:
 		$view->headTitle('FEGOS E-learning_Zend')->setSeparator(' :: ');
 
 		// Set the initial stylesheet:
-		//$view->headLink()->prependStylesheet('/elearning/public/css/');
+		// Set the initial stylesheet:
+        $view->headLink()->appendStylesheet('/elearning_zend/public/css/bootstrap.min.css');
+        $view->headLink()->appendStylesheet('/elearning_zend/public/css/camera.css');
+        $view->headLink()->appendStylesheet('/elearning_zend/public/css/style.css');
+        // $view->headLink()->appendStylesheet('/elearning_zend/public/');
+        
+        // Set the initial JS to load:
+        $view->headScript()->appendFile('/elearning_zend/public/js/jquery.min.js');
+        $view->headScript()->appendFile('/elearning_zend/public/js/camera.min.js');
+        $view->headScript()->appendFile('/elearning_zend/public/js/jquery.easing.1.3.js');
+        $view->headScript()->appendFile('/elearning_zend/public/js/jquery.mobile.customized.min.js');
+    }   
 
-		// Set the initial JS to load:
-		//$view->headScript()->prependFile('/elearning/public/js/');
-	}
+    protected function _initSession() {
+    	Zend_Session::start();
+    	$session = new Zend_Session_Namespace('Zend_Auth');
+    }
+	
 }
 
