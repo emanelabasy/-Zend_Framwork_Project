b83d00d298b56519f045cc5c316e1cbee007aac7
shrouk <shrouk.passiony@gmail.com>
2016-05-11 14:40:54 +0200

    layout by shrouk

--- elearning_zend/application/Bootstrap.php
+++ elearning_zend/application/Bootstrap.php
@@ -22,43 +22,43 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         //admin
         // if($layout == 'admin-layout'){
 
-            $view->headLink()->appendStylesheet('/course/public/css/style1.css');
-            $view->headLink()->appendStylesheet('/course/public/css/bootstrap.min1.css');
-            $view->headLink()->appendStylesheet('/course/public/css/icon-font.min.css');
-            $view->headLink()->appendStylesheet('/course/public/css/font-awesome.css');
-            $view->headLink()->appendStylesheet('/course/public/css?family=Montserrat:400,700');
-            $view->headLink()->appendStylesheet('/course/public/css?family=Roboto:700,500,300,100italic,100,400');
+            $view->headLink()->appendStylesheet('/z/-Zend_Framwork_Project/elearning_zend/public/css/style1.css');
+            $view->headLink()->appendStylesheet('/z/-Zend_Framwork_Project/elearning_zend/public/css/bootstrap.min1.css');
+            $view->headLink()->appendStylesheet('/z/-Zend_Framwork_Project/elearning_zend/public/css/icon-font.min.css');
+            $view->headLink()->appendStylesheet('/z/-Zend_Framwork_Project/elearning_zend/public/css/font-awesome.css');
+            $view->headLink()->appendStylesheet('/z/-Zend_Framwork_Project/elearning_zend/public/css?family=Montserrat:400,700');
+            $view->headLink()->appendStylesheet('/z/-Zend_Framwork_Project/elearning_zend/public/css?family=Roboto:700,500,300,100italic,100,400');
         
             //admin
-            $view->headScript()->appendFile('/course/public/js/amcharts.js');
-            $view->headScript()->appendFile('/course/public/js/serial.js');
-            $view->headScript()->appendFile('/course/public/js/light.js');
-            $view->headScript()->appendFile('/course/public/js/jquery-1.10.2.min.js');
-            $view->headScript()->appendFile('/course/public/js/owl.carousel.js');
-            $view->headScript()->appendFile('/course/public/js/jquery.flot.js');
-            $view->headScript()->appendFile('/course/public/js/jquery.nicescroll.js');
+            $view->headScript()->appendFile('/z/-Zend_Framwork_Project/elearning_zend/public/js/amcharts.js');
+            $view->headScript()->appendFile('/z/-Zend_Framwork_Project/elearning_zend/public/js/serial.js');
+            $view->headScript()->appendFile('/z/-Zend_Framwork_Project/elearning_zend/public/js/light.js');
+            $view->headScript()->appendFile('/z/-Zend_Framwork_Project/elearning_zend/public/js/jquery-1.10.2.min.js');
+            $view->headScript()->appendFile('/z/-Zend_Framwork_Project/elearning_zend/public/js/owl.carousel.js');
+            $view->headScript()->appendFile('/z/-Zend_Framwork_Project/elearning_zend/public/js/jquery.flot.js');
+            $view->headScript()->appendFile('/z/-Zend_Framwork_Project/elearning_zend/public/js/jquery.nicescroll.js');
           
-            $view->headScript()->appendFile('/course/public/js/scripts.js');
-            $view->headScript()->appendFile('/course/public/js/bootstrap.min.js');
-            $view->headScript()->appendFile('/course/public/js/jquery.flot.js');
-            $view->headScript()->appendFile('/course/public/js/jquery.fn.gantt.js');
-            $view->headScript()->appendFile('/course/public/js/menu_jquery.js');
+            $view->headScript()->appendFile('/z/-Zend_Framwork_Project/elearning_zend/public/js/scripts.js');
+            $view->headScript()->appendFile('/z/-Zend_Framwork_Project/elearning_zend/public/js/bootstrap.min.js');
+            $view->headScript()->appendFile('/z/-Zend_Framwork_Project/elearning_zend/public/js/jquery.flot.js');
+            $view->headScript()->appendFile('/z/-Zend_Framwork_Project/elearning_zend/public/js/jquery.fn.gantt.js');
+            $view->headScript()->appendFile('/z/-Zend_Framwork_Project/elearning_zend/public/js/menu_jquery.js');
 
         // }elseif($layout == 'layout') {
           
         
         
-        $view->headLink()->appendStylesheet('/course/public/css/bootstrap.min.css');
-        $view->headLink()->appendStylesheet('/course/public/css/camera.css');
-        $view->headLink()->appendStylesheet('/course/public/css/style.css');
+        $view->headLink()->appendStylesheet('/z/-Zend_Framwork_Project/elearning_zend/public/css/bootstrap.min.css');
+        $view->headLink()->appendStylesheet('/z/-Zend_Framwork_Project/elearning_zend/public/css/camera.css');
+        $view->headLink()->appendStylesheet('/z/-Zend_Framwork_Project/elearning_zend/public/css/style.css');
         
         
          
         // Set the initial JS to load:
-        $view->headScript()->appendFile('/course/public/js/jquery.min.js');
-        $view->headScript()->appendFile('/course/public/js/camera.min.js');
-        $view->headScript()->appendFile('/course/public/js/jquery.easing.1.3.js');
-        $view->headScript()->appendFile('/course/public/js/jquery.mobile.customized.min.js');
+        $view->headScript()->appendFile('/z/-Zend_Framwork_Project/elearning_zend/public/js/jquery.min.js');
+        $view->headScript()->appendFile('/z/-Zend_Framwork_Project/elearning_zend/public/js/camera.min.js');
+        $view->headScript()->appendFile('/z/-Zend_Framwork_Project/elearning_zend/public/js/jquery.easing.1.3.js');
+        $view->headScript()->appendFile('/z/-Zend_Framwork_Project/elearning_zend/public/js/jquery.mobile.customized.min.js');
         
         // }
     }   
